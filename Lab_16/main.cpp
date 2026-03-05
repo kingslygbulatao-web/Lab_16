dfdaf56eb09cca0814c79603f0a2de474e9db22a
Me <planetaryaxis93@gmail.com>
2026-03-05 11:22:52 -0800

    took class constructor code from lesson

--- Lab_16/main.cpp
+++ Lab_16/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Hello World" << endl;
+class color {
+private:
+    int red;
+    int green;
+    int blue;
 
-    return 0;
+public:
+    
+    color()                  { name = "none"; age = 0; }
+    color(string n)          { name = n; age = 0; }
+    color(string n, int a)   { name = n; age = a; }
+   
+    int getcolor()        { return red; }
+    void setcolor(string n)  { name = n; }
+    int getAge()            { return age; }
+    void setAge(int a)      { age = a; }
+};
+
+int main() {
+    Goat herd_1;
+    cout << herd_1.getName() << " " << herd_1.getAge() << endl;   
+    
+    Goat herd_2("Joe");
+    cout << herd_2.getName() << " " << herd_2.getAge() << endl;
+    
+    Goat herd_3("Jane", 12);
+    cout << herd_3.getName() << " " << herd_3.getAge() << endl;
+return 0;
 }
